31376ec34ad5c2041f4ee70d8625b2de670a6b45
pau suarez <pausuarez@MacBook-Air-de-pau.local>
2025-04-27 22:15:12 +0200

    Fix bot: Estable en Railway, actualizados requirements

--- bot.py
+++ bot.py
@@ -61,13 +61,7 @@ async def main():
     app.add_handler(CommandHandler('start', start))
 
     print("✅ Bot iniciado...")
-    await app.initialize()
-    await app.start()
-    await app.updater.start_polling()
-    await app.updater.idle()
+    await app.run_polling()
 
 if __name__ == "__main__":
-    import asyncio
-    loop = asyncio.get_event_loop()
-    loop.create_task(main())
-    loop.run_forever()
+    asyncio.run(main())
