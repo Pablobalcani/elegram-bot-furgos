3e8289de02df64a856f13c22cf3af0198baf05fe
pau suarez <pausuarez@MacBook-Air-de-pau.local>
2025-04-27 23:44:38 +0200

    Fix: corregido uso de coroutine awaitables correctamente

--- bot.py
+++ bot.py
@@ -26,7 +26,8 @@ async def buscar_ofertas(context: ContextTypes.DEFAULT_TYPE):
     resultados = []
 
     try:
-        resultados += buscar_milanuncios(MODELOS, PRECIO_MIN, PRECIO_MAX)
+        resultados_milanuncios = buscar_milanuncios(MODELOS, PRECIO_MIN, PRECIO_MAX)
+        resultados += resultados_milanuncios
 
         resultados_wallapop = await buscar_wallapop(MODELOS, PRECIO_MIN, PRECIO_MAX)
         resultados += resultados_wallapop
